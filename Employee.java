1cf61662044e34373f9ff070ba63537532ebfeae
PKRUNAL867 <prajapatipinki1112@gmail.com>
2024-05-21 12:54:28 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package myproject2;
+
+public class Employee {
+	private String name,email;
+
+	public String getName() {
+		return name;
+		
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	
+
+}
---
+++ demoemp.java
@@ -0,0 +1,15 @@
+package myproject2;
+
+public class demoemp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e1=new Employee();
+		e1.setName("pinki");
+		e1.setEmail("pinki@123");
+		System.out.println("name"+e1.getName());
+		System.out.println("age"+e1.getEmail());
+	}
+	
+}
